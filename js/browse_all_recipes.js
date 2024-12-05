d0fd5220780fbe64c65ad1c35af6bcfd3220798d
Ida <ida.starek@gmail.com>
2024-12-05 21:43:43 +0000

    connected to firebase DB

---
+++ js/browse_all_recipes.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://meal-suggestions-app-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const ingredientsInDB = ref(database, "ingredients");
+const recipesInDB = ref(database, "recipes");
+
