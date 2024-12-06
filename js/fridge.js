f0459c097682b3cb71ca55c086be8b9104a3e744
Ida <ida.starek@gmail.com>
2024-12-06 18:52:44 +0000

    connected fridge page to its js

--- js/fridge.js
+++ js/fridge.js
@@ -9,9 +9,3 @@ const app = initializeApp(appSettings);
 const database = getDatabase(app);
 const ingredientsInDB = ref(database, "ingredients");
 const recipesInDB = ref(database, "recipes");
-
-// fetch('../json/ingredients.json')
-//   .then(response => response.json())
-//   .then(data => console.log(data))
-//   .catch(error => console.error('Error:', error));
-
